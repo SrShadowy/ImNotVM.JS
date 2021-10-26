353da15b4b6921a21c500fc5e208fb63bd2cdc66
SrShadowy <43967231+SrShadowy@users.noreply.github.com>
2021-10-26 20:38:54 -0300

    Update

--- main_gets.js
+++ main_gets.js
@@ -22,7 +22,7 @@ let btn_i;
 
 
 
-
+let icons = ["folder.png", "web-browser-hd.png", "calc.png", "unknow.png", "logoOS.png"];
 
 
 
@@ -48,9 +48,40 @@ function context_menu_woow() {
 
 }
 
+
+
+function append_digito(e)
+{
+  //console.log("CALLME");
+
+  let calc_display = document.getElementById('Display'+e.name);
+  if(calc_display != undefined)
+  {
+	if(calc_display.value == 0)
+  		calc_display.value = e.value;
+  else
+			calc_display.value += e.value;
+  }
+}
+
+function calclu()
+{
+  let calc_display = document.getElementById('Display'+e.name);
+  if(calc_display != undefined)
+  {
+    var x = eval(calc_display.value);
+    calc_display.value = x;
+  }
+
+}
+
+
+
 let last_top_div;
 
 function divd(type) {
+
+  var type_img = 0;
   var windows = document.createElement('div');
   windows.id = "new_window" + i_divs;
   ++i_divs;
@@ -87,15 +118,57 @@ function divd(type) {
   defaults();
 
   if (type != undefined) {
-    var new_componete = document.createElement(type);
-    new_componete.style.top = "25px";
-    new_componete.style.width = "99%";
-    new_componete.style.height = "90%";
-    new_componete.style.overflow = 'auto';
-    new_componete.src = "https://srshadowy.github.io/Teclado_Sonoro/";
-    windows.appendChild(new_componete);
+    switch(type)
+    {
+      case "calc":
+      {
+        var calcs = document.getElementById('calc').innerHTML;
+        var x = document.createElement('div');
+        x.innerHTML = calcs;
+        x.id = "calc";//+i_divs;
+        x.childNodes[1].childNodes[1].id = "Display"+i_divs;
+        x.childNodes[1].childNodes[1].style.width = "90%";
+        x.childNodes[1].childNodes[1].style.height = "50px";
+        x.childNodes[1].childNodes[1].style.textAlign = "end";
+        x.childNodes[1].childNodes[1].style.backgroundColor = "black";
+        x.childNodes[1].childNodes[1].style.color = "white";
+
+        for( i = 0; i < x.childNodes[3].childNodes.length; i++)
+        {
+          if( x.childNodes[3].childNodes[i].value != undefined)
+          {  
+            x.childNodes[3].childNodes[i].name = i_divs; 
+          }
+        }
+
+        //console.log(x.childNodes.length);
+        //calcs.style.visibility = "visible";
+
+
+        windows.appendChild(x);
+        type_img = 2;
+        windows.style.width = "260px";
+        windows.style.height = "370px";
+      }
+      break;
+
+      case "iframe":
+        windows.style.overflow = "hidden";
+        windows.style.overflowX = "auto";
+        var new_componete = document.createElement(type);
+        new_componete.style.top = "25px";
+        new_componete.style.width = "inherit";
+        new_componete.style.height = "inherit";
+        new_componete.style.overflow = 'auto';
+        new_componete.src = "https://srshadowy.github.io/Teclado_Sonoro/";
+        windows.appendChild(new_componete);
+        type_img = 1;
+      break;
+    }
+
   }else
   {
+    type_img = 0;
     windows.style.color = "white";
     //windows.innerHTML += ("MY PROJECTS");
 
@@ -165,7 +238,7 @@ function divd(type) {
   document.getElementById('dd.h').appendChild(windows);
   close.onclick = function (e) {
     e.preventDefault();
-    console.log("feixa");
+    //console.log("feixa");
     var x = document.getElementById(close.id);
     x.parentNode.removeChild(x);
   }
@@ -187,10 +260,11 @@ function divd(type) {
     xopen.style.width = "40px";
     xopen.style.height = "40px";
     xopen.style.color = 'white';
-    xopen.style.backgroundColor = "royalblue";
+    //xopen.style.backgroundColor = "black";
     xopen.value = 'Win';
     xopen.style.backgroundSize = "cover";
-    xopen.style.backgroundImage = "url('./icons/folder.png')";
+    var lets_img = ( "url('./icons/" +  icons[type_img] +"')");
+    xopen.style.backgroundImage = lets_img;
     xopen.onclick = function()
     {
       windows.style.visibility = 'visible';
@@ -300,6 +374,7 @@ function init() {
 
   btn_i.onclick = function (e) {
     if (document.getElementById("iniciar") == null) {
+
       var iniciar = document.createElement('div');
       iniciar.id = "iniciar";
       iniciar.style.position = "fixed";
@@ -329,6 +404,46 @@ function init() {
       }
 
       iniciar.appendChild(btn);
+
+      var btn = document.createElement('input');
+      btn.id = iniciar.id;
+      btn.type = "button"
+      btn.style.left = "0px";
+      btn.style.top = "30px";
+      btn.style.width = "200px";
+      btn.style.height = "30px";
+      //btn.name = "btn_IFRAME";
+      btn.value = "calculadora :)";
+      btn.style.color = "red";
+      btn.style.backgroundColor = "blue";
+      btn.onclick = function (e) {
+        divd("calc");
+        //window.open("https://github.com/SrShadowy", '_blank');
+        var elem = document.getElementById("iniciar");
+        elem.parentNode.removeChild(elem);
+      }
+
+      iniciar.appendChild(btn);
+      var btn = document.createElement('input');
+      btn.id = iniciar.id;
+      btn.type = "button"
+      btn.style.left = "0px";
+      btn.style.top = "60px";
+      btn.style.width = "200px";
+      btn.style.height = "30px";
+      //btn.name = "btn_IFRAME";
+      btn.value = "Vazio :)";
+      btn.style.color = "red";
+      btn.style.backgroundColor = "blue";
+      btn.onclick = function (e) {
+        divd("vazio");
+        //window.open("https://github.com/SrShadowy", '_blank');
+        var elem = document.getElementById("iniciar");
+        elem.parentNode.removeChild(elem);
+      }
+
+      iniciar.appendChild(btn);
+
       main.appendChild(iniciar);
     } else {
       var elem = document.getElementById("iniciar");
